2f12736b4d8e963c0a046b03e281ab9b72806c58
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-07-15 19:57:36 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# md2html
+
+Developed through the 25th week of my **Project52**, this is a simple Ruby script that converts a Markdown file into an HTML file. It is not intended to be used as a production-ready gem; [Redcarpet](https://github.com/vmg/redcarpet) is a better version of this (or in fact, this repo is a bad version of Redcarpet).
--- main.rb
+++ main.rb
@@ -5,15 +5,12 @@ if ARGV.count != 2
   exit
 else
   md_filename = ARGV[0]
-  # html_filename = ARGV[1]
+  html_filename = ARGV[1]
 end
 
-# md_file = File.open(md_filename)
 md_content = IO.read(md_filename)
 
 md_html_converter = MdHtml.new
 html_content = md_html_converter.to_html md_content
 
-# html_file = File.open(html_filename, "w")
-
-puts html_content
+File.open(html_filename, "w") {|file| file.write html_content}
